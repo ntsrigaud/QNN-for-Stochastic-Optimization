099109f0790507359ad61a8e5d2932043c0402e4
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2026-06-19 02:12:45 +0800

    build: expose QNN framework utility helpers at package root

--- src/qnn_stoch_opt/utils/__init__.py
+++ src/qnn_stoch_opt/utils/__init__.py
@@ -1,3 +1,19 @@
 """
-Helper functions and utilities.
+Utility helpers for the QNN framework.
+
+Exports:
+    compute_quantile_crossing_rate: Fraction of samples that exhibit quantile
+        crossing violations in a set of QNN predictions.
+    compute_optimality_gap: Relative optimality gap between a heuristic objective
+        and a reference (e.g. SAA) objective value.
 """
+
+from qnn_stoch_opt.utils.metrics import (
+    compute_optimality_gap,
+    compute_quantile_crossing_rate,
+)
+
+__all__ = [
+    "compute_quantile_crossing_rate",
+    "compute_optimality_gap",
+]
